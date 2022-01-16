f0e00dd6442ef2760cbf700cc3bb03f6a8c8aef1
Michael Freeman <44925374+freemanm65@users.noreply.github.com>
2022-01-16 14:32:16 +0000

    Update genComposites.py
    Use correct limit for i

--- genComposites.py
+++ genComposites.py
@@ -4,7 +4,7 @@ import math
 def triangle(limit):
 	composites = {9}
 	lim = int((limit/3-1)/2)
-	for i in range(1,int(math.sqrt(limit))+1):
+	for i in range(1, lim+1):
 		for j in range(1,i+1):
 			if (2*i+1)*(2*j+1) < limit:
 				composites.add((2*i+1)*(2*j+1))
